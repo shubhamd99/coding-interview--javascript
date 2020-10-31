14e2e719506a12d9bc6ee02500c13c7c1cbc5db4
Shubham Dhage <shubhamd@rigbot.com>
2020-10-31 15:16:24 +0530

    add logic for level-width porblem

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -15,6 +15,13 @@
 // 4       5
 // Answer: [1, 3, 2]
 
+// 0 -> data: data, childrens = [1,2,3]
+// 1 => data: data, childrens = [4]
+// 2 => data: data, childrens = []
+// 3 => data: data, childrens = [5]
+// 4 => data: data, childrens = []
+// 5 => data: data, childrens = []
+
 function levelWidth(root) {
     const arr = [root, 's']; // 's' is stopper
     const counters = [0]; // levels of the tree
@@ -33,5 +40,25 @@ function levelWidth(root) {
 
     return counters; // [1, 1, 2, 1]
 }
+
+// Solution Logic:
+// arr = [0th root node, 's'], counter = [0]
+// start loop
+// arr = ['s', 1st node, 2nd node, 3rd node], counter = [1]
+// second iteration
+// arr = [1st node, 2nd node, 3rd node,'s'], counter = [1, 0]
+// third iteration
+// arr = [2nd node, 3rd node,'s', 4th node], counter = [1, 1]
+// third iteration
+// arr = [3rd node,'s', 4th node], counter = [1, 2]
+// fourth iteration
+// arr = ['s', 4th, 5th node], counter = [1, 3]
+// fourth iteration
+// arr = [4, 5,'s'], counter = [1, 3, 0]
+// fifth iteration
+// arr = [5,'s'], counter = [1, 3, 1]
+// sixth iteration
+// arr = ['s'], counter = [1, 3, 2]
+// loop stopped (arr.length > 1)
 
 module.exports = levelWidth;
